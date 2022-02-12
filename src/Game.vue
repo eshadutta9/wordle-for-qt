4da6599949d6ed7caaa0f91b5d8eada80cb5f387
eshadutta9 <45624572+eshadutta9@users.noreply.github.com>
2022-02-13 01:21:17 +0530

    Update Game.vue

--- src/Game.vue
+++ src/Game.vue
@@ -113,7 +113,7 @@ function completeRow() {
       setTimeout(() => {
         grid = genResultGrid()
         showMessage(
-            'Happy Valentine\'s Day! Will you be my _____',
+            'Happy Valentine\'s Day, Adithya! You are a _____ pie',
           //['Genius', 'Magnificent', 'Impressive', 'Splendid', 'Great', 'Phew'][
           //  currentRowIndex
           //],
